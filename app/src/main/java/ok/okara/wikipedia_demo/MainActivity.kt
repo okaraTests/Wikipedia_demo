38d616bfa95c931cf932ad363b8942e8aa39a411
ok <okaramy94@gmail.com>
2023-06-23 23:07:29 +0330

    first commit

--- app/src/main/java/ok/okara/wikipedia_demo/MainActivity.kt
+++ app/src/main/java/ok/okara/wikipedia_demo/MainActivity.kt
@@ -2,10 +2,12 @@ package ok.okara.wikipedia_demo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Toast.makeText(this, "hello first commit", Toast.LENGTH_SHORT).show()
     }
 }
