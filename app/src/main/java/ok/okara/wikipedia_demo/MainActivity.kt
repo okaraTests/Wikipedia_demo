01633ce67acc9219a96672c563bb946d056b818d
ok <okaramy94@gmail.com>
2023-06-23 23:13:10 +0330

    first commit

--- app/src/main/java/ok/okara/wikipedia_demo/MainActivity.kt
+++ app/src/main/java/ok/okara/wikipedia_demo/MainActivity.kt
@@ -3,11 +3,24 @@ package ok.okara.wikipedia_demo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import ok.okara.wikipedia_demo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        Toast.makeText(this, "hello first commit", Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, "hello first commit", Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, "hello first commit", Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, "hello first commit", Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, "hello first commit", Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, "hello first commit", Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, "hello first commit", Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, "hello first commit", Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, "hello first commit", Toast.LENGTH_SHORT).show()
         Toast.makeText(this, "hello first commit", Toast.LENGTH_SHORT).show()
     }
 }
